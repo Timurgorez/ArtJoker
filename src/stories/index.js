ba94ffb1edee794384dfa7124f0356aadfa8fe41
Timurgorez <timuumit@mail.ru>
2018-05-10 21:56:10 +0300

    Update index.js

--- src/stories/index.js
+++ src/stories/index.js
@@ -10,17 +10,4 @@ import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import Header from '../header';
 
 
-//
-// storiesOf('Welcome', module).add('to Storybook', () => <Welcome showApp={linkTo('Button')} />);
-//
-// storiesOf('Button', module)
-//   .add('with text', () => <Button onClick={action('clicked')}>Hello Button</Button>)
-//   .add('with some emoji', () => (
-//     <Button onClick={action('clicked')}>
-//       <span role="img" aria-label="so cool">
-//         😀 😎 👍 💯
-//       </span>
-//     </Button>
-//   ));
-
 storiesOf('ArtJoker', module).add('first', () => <MuiThemeProvider><Header /></MuiThemeProvider> );
